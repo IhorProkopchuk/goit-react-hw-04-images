9d75fbf5d59c68d3d5a9a245a02f76d4f1004492
IhorProkopchuk <rectoon001@gmail.com>
2023-05-16 16:21:14 +0300

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { fetchImages } from '../api/fetchImage';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -8,133 +8,92 @@ import { Loader } from '../components/Loader/Loader';
 
 import styles from './App.module.css';
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    error: null,
-    isLoading: false,
-    page: 1,
-    totalHits: 0,
-    showModal: false,
-    activeImg: null,
-  };
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalHits, setTotalHits] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [activeImg, setActiveImg] = useState(null);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-
-    if (prevState.query !== query) {
-      this.resetState();
-      this.fetchImages(query, 1);
-    } else if (prevState.page !== page && page !== 1) {
-      this.fetchImages(query, page);
+  useEffect(() => {
+    if (query !== '') {
+      fetchImageData(query, page);
     }
-  }
+  }, [query, page]);
 
-  resetState = () => {
-    this.setState({
-      page: 1,
-      images: [],
-      error: null,
-    });
+  const resetState = () => {
+    setPage(1);
+    setImages([]);
+    setError(null);
   };
 
-  // fetchImages = async (query, page) => {
-  //   try {
-  //     this.setState({ isLoading: true });
-  //     const imgPixabay = await fetchImages(query, page);
-
-  //     if (imgPixabay.totalHits > 0) {
-  //       this.setState(({ images, page }) => ({
-  //         images: [...images, ...imgPixabay.hits],
-  //         totalHits: imgPixabay.totalHits,
-  //       }));
-  //     } else {
-  //       this.setState({
-  //         error: 'No images matching your search query',
-  //       });
-  //     }
-  //   } catch {
-  //     this.setState({ error: 'No pictures were founded' });
-  //   } finally {
-  //     this.setState({ isLoading: false });
-  //   }
-  // };
-
-  fetchImages = (query, page) => {
-    this.setState({ isLoading: true });
+  const fetchImageData = (query, page) => {
+    setIsLoading(true);
     fetchImages(query, page)
       .then(imgPixabay => {
         if (imgPixabay.totalHits > 0) {
-          this.setState(({ images, page }) => ({
-            images: [...images, ...imgPixabay.hits],
-            totalHits: imgPixabay.totalHits,
-          }));
+          setImages(prevImages => [...prevImages, ...imgPixabay.hits]);
+          setTotalHits(imgPixabay.totalHits);
         } else {
-          this.setState({
-            error: 'No images matching your search query',
-          });
+          setError('No images matching your search query');
         }
       })
       .catch(() => {
-        this.setState({ error: 'No pictures were founded' });
+        setError('No pictures were found');
       })
       .finally(() => {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       });
   };
 
-  onSubmit = event => {
+  const onSubmit = event => {
     event.preventDefault();
     const { value } = event.target.elements.input;
     const query = value.trim();
 
     if (query === '') {
-      this.setState({ error: 'Search field can not be empty' });
+      setError('Search field can not be empty');
     } else {
-      this.setState({ query, error: null });
+      setQuery(query);
+      resetState();
+      setError(null);
     }
   };
 
-  nextPage = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const nextPage = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  openModal = () => {
-    this.setState({ showModal: true });
+  const openModal = () => {
+    setShowModal(true);
   };
 
-  closeModal = () => {
-    this.setState({ showModal: false });
+  const closeModal = () => {
+    setShowModal(false);
   };
 
-  handleImgClick = activeImg => {
-    this.setState({ activeImg: { ...activeImg } });
-    this.openModal();
+  const handleImgClick = activeImg => {
+    setActiveImg({ ...activeImg });
+    openModal();
   };
 
-  render() {
-    const { error, images, isLoading, showModal, activeImg } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.onSubmit} />
-        {isLoading && <Loader className={styles.loader} />}
-        {showModal && (
-          <Modal activeImg={activeImg} closeModal={this.closeModal} />
-        )}
-        {error && <div className={styles.error}>{error}</div>}
-        {images && (
-          <ImageGallery handleImgClick={this.handleImgClick} images={images} />
-        )}
-        {!isLoading &&
-          !error &&
-          images &&
-          this.state.totalHits > images.length && (
-            <Button nextPage={this.nextPage} />
-          )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Searchbar onSubmit={onSubmit} />
+      {isLoading && <Loader className={styles.loader} />}
+      {showModal && <Modal activeImg={activeImg} closeModal={closeModal} />}
+      {error && <div className={styles.error}>{error}</div>}
+      {images && (
+        <ImageGallery handleImgClick={handleImgClick} images={images} />
+      )}
+      {!isLoading && !error && images && totalHits > images.length && (
+        <Button nextPage={nextPage} />
+      )}
+    </>
+  );
+};
+
+export default App;
